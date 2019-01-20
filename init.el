c0b525f4a15e0bf30da36d96b6f0f548aa217779
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2019-01-20 02:59:45 -0200

    Many enhancements.

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 ;; declare required packages
-(setq package-list '(evil evil-surround ace-jump-mode js2-mode discover ujelly-theme smartparens nlinum))
+(setq package-list '(ace-jump-mode company company-quickhelp discover elpy evil evil-surround exec-path-from-shell go-mode helm js2-mode magit nlinum smartparens ujelly-theme which-key))
 
 ;; disable all menu bars
 (menu-bar-mode -1)
@@ -14,16 +14,16 @@
 (setq package-archives '())
 
 (add-to-list 'package-archives
-             '("elpa" . "http://tromey.com/elpa/"))
+	     '("elpa" . "http://tromey.com/elpa/"))
 
 (add-to-list 'package-archives
-             '("gnu" . "http://elpa.gnu.org/packages/"))
+	     '("gnu" . "http://elpa.gnu.org/packages/"))
 
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (add-to-list 'package-archives
-             '("melpa" . "http://stable.melpa.org/packages/") t)
+	     '("melpa" . "http://stable.melpa.org/packages/") t)
 
 ;; update all package repositories unless all required packages are installed and archive contents are available
 (setq refresh-contents-needed nil)
@@ -39,15 +39,6 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-;; enable vim emulation
-(require 'evil)
-(require 'evil-surround)
-(evil-mode 1)
-(global-evil-surround-mode 1)
-
-;; ace-jump
-(autoload 'ace-jump-mode "ace-jump-mode" "Emacs quick minor move mode" t)
-
 ;; start emacs on a new buffer
 (setq inhibit-splash-screen t)
 (switch-to-buffer "**")
@@ -55,6 +46,20 @@
 ;; load a nice colour theme
 (require 'ujelly-theme)
 (load-theme 'ujelly t)
+
+;; enable vim emulation
+(require 'evil)
+(require 'evil-surround)
+(evil-mode 1)
+(global-evil-surround-mode 1)
+
+;; auto-completion framework
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(company-quickhelp-mode)
+
+;; ace-jump
+(autoload 'ace-jump-mode "ace-jump-mode" "Emacs quick minor move mode" t)
 
 ;; discover.el
 (require 'discover)
@@ -64,51 +69,76 @@
 (require 'smartparens-config)
 (smartparens-global-mode t)
 
-;; define useful functions
+;; enable line numbers
+(global-nlinum-mode t)
+
+;; elpy
+(elpy-enable)
+(setq elpy-rpc-python-command "python3")
+
+;; which-key
+(require 'which-key)
+(which-key-mode)
+
+;; helm
+(require 'helm)
+
+;; golang
+(setenv "GOPATH" (format "%s/go" (getenv "HOME")))
+(load "$GOPATH/src/github.com/stamblerre/gocode/emacs-company/company-go.el")
+(add-hook 'go-mode-hook (lambda ()
+			  (set (make-local-variable 'company-backends) '(company-go))
+			  (company-mode)))
+
+;; personalised goodies
 (defun kill-other-buffers ()
   "Kill all other buffers."
   (interactive)
   (mapc 'kill-buffer (delq (current-buffer) (buffer-list)))
   (message "Killed all other buffers!"))
 
+(defun go-to-buffer-or-exec-subprocess (new-buffer-name command)
+  "Create a function that switches to a buffer running the specified subprocess, powered by ansi-term. If the current buffer is the subprocess's, switch to the previous buffer."
+  `(lambda ()
+     (interactive)
+     (let ((unassociated-new-buffer-name (format "*%s*" ,new-buffer-name)))
+       (if (string-equal (buffer-name) unassociated-new-buffer-name)
+	   (previous-buffer)
+	 (if (get-buffer unassociated-new-buffer-name)
+	     (switch-to-buffer unassociated-new-buffer-name)
+	   (ansi-term ,command ,new-buffer-name))))))
+
 ;; define useful keymaps
-(global-set-key (kbd "s-!") 'kill-other-buffers)
-(global-set-key (kbd "s-'") 'shell)
-(global-set-key (kbd "s-`") 'shell)
-(global-set-key (kbd "s-,") 'widen)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "s-x") 'helm-M-x)
+(global-set-key (kbd "s-q") 'keyboard-quit)
+(global-set-key (kbd "s-'") (go-to-buffer-or-exec-subprocess "shell" "/bin/bash"))
 (global-set-key (kbd "s-.") 'narrow-to-region)
+(global-set-key (kbd "s-,") 'widen)
+(global-set-key (kbd "s--") 'text-scale-adjust)
+(global-set-key (kbd "s-=") 'text-scale-adjust)
 (global-set-key (kbd "s-/") 'comment-dwim)
 (global-set-key (kbd "s-;") 'comment-dwim)
 (global-set-key (kbd "s-SPC") 'ace-jump-mode)
-(global-set-key (kbd "s-a") 'find-file)
-(global-set-key (kbd "s-=") 'text-scale-adjust)
-(global-set-key (kbd "s--") 'text-scale-adjust)
-(global-set-key (kbd "s-e") 'eval-expression)
-(global-set-key (kbd "s-r") 'eval-buffer)
-(global-set-key (kbd "s-x") 'execute-extended-command)
-(global-set-key (kbd "s-q") 'keyboard-quit)
-(global-set-key (kbd "s-h k") 'describe-key)
+
+(global-set-key (kbd "s-<") 'switch-to-prev-buffer)
+(global-set-key (kbd "s->") 'switch-to-next-buffer)
+(global-set-key (kbd "s-S-<up>") 'split-window-vertically)
+(global-set-key (kbd "s-S-<down>") 'split-window-vertically)
+(global-set-key (kbd "s-S-<left>") 'split-window-horizontally)
+(global-set-key (kbd "s-S-<right>") 'split-window-horizontally)
+(global-set-key (kbd "s-<up>") 'windmove-up)
+(global-set-key (kbd "s-<down>") 'windmove-down)
+(global-set-key (kbd "s-<left>") 'windmove-left)
+(global-set-key (kbd "s-<right>") 'windmove-right)
+
+(global-set-key (kbd "s-h a") 'apropos)
 (global-set-key (kbd "s-h b") 'describe-bindings)
+(global-set-key (kbd "s-h k") 'describe-key)
 
-;; enable line numbers
-(global-nlinum-mode t)
-
-;; custom
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("4a888176c950a6716b10312cf5878dfb01a57e1b8c0e902590fe9c00b4ab8955" default)))
- '(package-selected-packages
-   (quote
-    (nlinum smartparens discover ujelly-theme js2-mode ace-jump-mode evil-surround evil))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'narrow-to-region 'disabled nil)
+(global-set-key (kbd "s-b l") 'helm-buffers-list)
+(global-set-key (kbd "s-b k") 'kill-buffer)
+(global-set-key (kbd "s-b o") 'kill-other-buffers)
+(global-set-key (kbd "s-w k") 'delete-window)
+(global-set-key (kbd "s-w o") 'delete-other-windows)
+(global-set-key (kbd "s-f f") 'helm-find-files)
