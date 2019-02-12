9d61a1333f47152267f41f1cef4e74cc220e2837
Martin Jungblut Schreiner <martin.jungblut.schreiner@hp.com>
2019-02-12 15:23:13 -0200

    Initial JavaScript support.

--- init.el
+++ init.el
@@ -11,12 +11,14 @@
 (add-to-list 'package-list 'go-mode)
 (add-to-list 'package-list 'helm)
 (add-to-list 'package-list 'js2-mode)
+(add-to-list 'package-list 'js2-refactor)
 (add-to-list 'package-list 'magit)
 (add-to-list 'package-list 'markdown-mode)
 (add-to-list 'package-list 'nlinum)
 (add-to-list 'package-list 'smartparens)
 (add-to-list 'package-list 'ujelly-theme)
 (add-to-list 'package-list 'which-key)
+(add-to-list 'package-list 'xref-js2)
 
 ;; declare package repositories
 (setq package-archives '())
@@ -117,6 +119,24 @@
 ;; Python
 (elpy-enable)
 (setq elpy-rpc-python-command "python3")
+
+;; JavaScript
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
+
+(require 'js2-refactor)
+(require 'xref-js2)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "s-r")
+;; (define-key js2-mode-map (kbd "C-k") #'js2r-kill)
+
+;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
+;; unbind it.
+;; (define-key js-mode-map (kbd "M-.") nil)
+
+(add-hook 'js2-mode-hook (lambda () (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 ;;;; language specific ;;;;
 
 
