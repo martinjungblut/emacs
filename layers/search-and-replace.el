8ce29a7ca78ff4abbd38b28e2aec35c82488d27a
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2019-05-12 22:44:09 -0300

    search-and-replace--with-buffer is now a macro.
    My first elisp macro.

--- layers/search-and-replace.el
+++ layers/search-and-replace.el
@@ -1,19 +1,19 @@
-(defun search-and-replace--with-buffer (body...)
-  (let ((search-and-replace--buffer-name "*hgrep*"))
+(defmacro search-and-replace--with-buffer (&rest body)
+  `(let ((search-and-replace--buffer-name "*hgrep*"))
     (if (get-buffer search-and-replace--buffer-name)
 	(with-current-buffer search-and-replace--buffer-name
-	  (eval body...)))))
+	  (progn ,@body)))))
 
 (defun search-and-replace--start (arg)
   (interactive "P")
   (search-and-replace--with-buffer
-   '(kill-buffer-and-window))
+   (kill-buffer-and-window))
   (helm-do-grep-ag arg)
   (search-and-replace--with-buffer
-   '(wgrep-change-to-wgrep-mode)))
+   (wgrep-change-to-wgrep-mode)))
 
 (defun search-and-replace--commit ()
   (interactive)
   (wgrep-finish-edit)
   (search-and-replace--with-buffer
-   '(kill-buffer-and-window)))
+   (kill-buffer-and-window)))
