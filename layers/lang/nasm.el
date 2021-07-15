42c0c29b1c9dbe4a81f01b6b7484498a7d72eb4f
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2021-07-15 14:02:40 -0300

    Use spaces rather than tabs for asm code.

--- layers/lang/nasm.el
+++ layers/lang/nasm.el
@@ -1,3 +1,7 @@
+(defun asm-hook-indentation ()
+    (setq-local indent-tabs-mode nil))
+
 (use-package nasm-mode
   :ensure t
-  :config (add-hook 'asm-mode-hook 'nasm-mode))
+  :config (add-hook 'asm-mode-hook 'nasm-mode)
+          (add-hook 'asm-mode-hook 'asm-hook-indentation))
