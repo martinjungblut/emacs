a88f6241e5307d07552fd9ec689b3fe9ef78bab0
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2021-09-03 02:36:54 -0300

    Replace tabs with spaces.

--- layers/features/autokill-buffers.el
+++ layers/features/autokill-buffers.el
@@ -1,10 +1,10 @@
 (defun autokill-buffers ()
   (let ((regexes '("magit.*")))
-	(dolist (regex regexes)
-	  (dolist (buf (buffer-list))
-		(if (string-match regex (buffer-name buf))
-			(progn
-			  (message (format "Automatically killing buffer: %s" (buffer-name buf)))
-			  (kill-buffer buf)))))))
+    (dolist (regex regexes)
+      (dolist (buf (buffer-list))
+        (if (string-match regex (buffer-name buf))
+            (progn
+              (message (format "Automatically killing buffer: %s" (buffer-name buf)))
+              (kill-buffer buf)))))))
 
 (add-hook 'after-save-hook 'autokill-buffers)
