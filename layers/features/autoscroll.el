8251b157daee77ad7e9339c5fd8d4b4ac83aac71
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-05-05 22:08:12 -0300

    Fix broken autoscroll.

--- layers/features/autoscroll.el
+++ layers/features/autoscroll.el
@@ -1,22 +1,10 @@
-;; (setq recenter-positions '(middle))
-
-(setq current-buffer-name (buffer-name))
-
-(defun update-buffer-name ()
-  (setq current-buffer-name (buffer-name)))
-
-(add-hook 'post-command-hook #'update-buffer-name)
-
 (setq current-line-number (line-number-at-pos))
 
 (defun autoscroll ()
-  (let ((new-line-number (line-number-at-pos))
-		(new-buffer-name (buffer-name)))
-	(when
-		(and
-		 (not (equal new-line-number current-line-number))
-		 (equal new-buffer-name current-buffer-name))
+  (let ((new-line-number (line-number-at-pos)))
+	(when (not (equal new-line-number current-line-number))
 	  (setq current-line-number new-line-number)
-	  (recenter-top-bottom))))
+	  ;; recenter must not be called directly, else magit is broken
+	  (run-at-time nil nil #'recenter))))
 
 (add-hook 'post-command-hook #'autoscroll)
