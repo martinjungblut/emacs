059f661e28b4577b31c6c655f837b8a3998bb1f7
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-08-24 18:56:53 -0300

    Do not call garbage-collect when Emacs goes out of focus.

--- layers/performance.el
+++ layers/performance.el
@@ -3,5 +3,4 @@
   :config (gcmh-mode +1)
   :init
   (add-hook 'minibuffer-setup-hook #'gcmh-set-high-threshold)
-  (add-hook 'minibuffer-setup-hook #'gcmh-register-idle-gc)
-  (add-hook 'focus-out-hook #'garbage-collect))
+  (add-hook 'minibuffer-setup-hook #'gcmh-register-idle-gc))
