ba064a6f3455df08bdde4cc77f9fb692cb9ee252
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-08-25 15:48:06 -0300

    Change theme and font size.

--- layers/03-font.el
+++ layers/03-font.el
@@ -25,4 +25,4 @@
 	   (set-face-attribute 'default nil :height ,font-height)
 	   (message (format "Default font height: %d" (font-height-get))))))
 
-(apply-font-settings "Inconsolata" 'regular 125 5)
+(apply-font-settings "Inconsolata" 'regular 150 5)
