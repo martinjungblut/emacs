7e7d8211898cbaa833d06df031d3bc13acc4ab5d
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2022-11-02 14:32:31 +0000

    Improve GCMH settings.

--- layers/performance.el
+++ layers/performance.el
@@ -1,6 +1,4 @@
 (use-package gcmh
   :ensure t
   :config (gcmh-mode +1)
-  :init
-  (add-hook 'minibuffer-setup-hook #'gcmh-set-high-threshold)
-  (add-hook 'minibuffer-setup-hook #'gcmh-register-idle-gc))
+  :init)
