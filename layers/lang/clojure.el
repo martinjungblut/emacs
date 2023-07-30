74b0ba915785c770b5d1e4d654ac15ba242c91fd
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2023-07-30 03:05:11 -0300

    Clojure: Automatically bring CIDER up.

--- layers/lang/clojure.el
+++ layers/lang/clojure.el
@@ -1,5 +1,10 @@
 (use-package clojure-mode
-  :ensure t)
+  :ensure t
+  :config (add-hook 'clojure-mode-hook 'clojure-hook))
 
 (use-package cider
   :ensure t)
+
+(defun clojure-hook ()
+  (progn
+	(cider-jack-in '())))
