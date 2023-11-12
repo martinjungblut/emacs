9e3c9903a3381026206a06c0c1990ea5105ba18d
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2023-11-12 19:26:44 -0300

    Add extra Go dep: gogtags.

--- extra-deps/go.sh
+++ extra-deps/go.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-go install -v golang.org/x/tools/gopls@latest
 go install -v github.com/cweill/gotests@latest
 go install -v github.com/davidrjenni/reftools/cmd/fillstruct@latest
 go install -v github.com/fatih/gomodifytags@latest
@@ -9,6 +8,7 @@ go install -v github.com/godoctor/godoctor@latest
 go install -v github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 go install -v github.com/haya14busa/gopkgs/cmd/gopkgs@latest
 go install -v github.com/josharian/impl@latest
+go install -v github.com/juntaki/gogtags@latest
 go install -v github.com/mdempsky/gocode@latest
 go install -v github.com/rogpeppe/godef@latest
 go install -v github.com/zmb3/gogetdoc@latest
@@ -17,3 +17,4 @@ go install -v golang.org/x/tools/cmd/godoc@latest
 go install -v golang.org/x/tools/cmd/goimports@latest
 go install -v golang.org/x/tools/cmd/gorename@latest
 go install -v golang.org/x/tools/cmd/guru@latest
+go install -v golang.org/x/tools/gopls@latest
