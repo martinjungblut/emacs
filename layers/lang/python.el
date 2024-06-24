ff731ccb11b624db51b921431e7b144ebd487ae1
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2024-06-24 17:17:01 -0300

    Improve Python debugging support.

--- layers/lang/python.el
+++ layers/lang/python.el
@@ -1,3 +1,7 @@
+(defun python-locate-interpreter ()
+  (let ((located-interpreter (car (f-glob (format "%s*/bin/%s" (fs-get-project-directory) "python")))))
+    (if located-interpreter located-interpreter "/usr/bin/python3")))
+
 (use-package lsp-pyright
   :ensure t
   :after lsp-mode
@@ -13,4 +17,12 @@
 (use-package pyimport
   :ensure t)
 
+(defun dap-mode-python ()
+  (require 'dap-python)
+  ;; if you installed debugpy, you need to set this
+  ;; https://github.com/emacs-lsp/dap-mode/issues/306
+  (setq dap-python-debugger 'debugpy)
+  (setq dap-python-executable (python-locate-interpreter)))
+
 (add-hook 'python-mode-hook 'helm-gtags-mode)
+(add-hook 'python-mode-hook 'dap-mode-python)
