daa0d26d94e3afb3d9de84225c35e0f22e987cfb
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2024-10-24 16:57:03 -0300

    Update font.

--- layers/03-font.el
+++ layers/03-font.el
@@ -25,4 +25,4 @@
 	   (set-face-attribute 'default nil :height ,font-height)
 	   (message (format "Default font height: %d" (font-height-get))))))
 
-(apply-font-settings "Source Code Pro" 'regular 100 10)
+(apply-font-settings "Source Code Pro" 'regular 125 5)
