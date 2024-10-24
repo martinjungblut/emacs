c8361d1522605b5dba2ab01df505ef4582240b87
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2024-10-24 16:54:01 -0300

    Update PlantUML JAR path.

--- layers/lang/plantuml.el
+++ layers/lang/plantuml.el
@@ -1,5 +1,5 @@
 (use-package plantuml-mode
   :ensure t
   :config (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
-	  (setq plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
+	  (setq plantuml-jar-path "/usr/share/java/plantuml.jar")
 	  (setq plantuml-output-type "png"))
