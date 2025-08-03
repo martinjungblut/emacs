0e9302f0528b8359bcff3a3b64fd88bf47b6abf7
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2025-08-03 14:49:16 -0300

    Add benchmark-init.

--- layers/01-package-management.el
+++ layers/01-package-management.el
@@ -37,3 +37,9 @@
 ;;       (goto-char (point-max))
 ;;       (eval-print-last-sexp)))
 ;;   (load bootstrap-file nil 'nomessage))
+
+(use-package benchmark-init
+  :ensure t
+  :config
+  ;; To disable collection of benchmark data after init is done.
+  (add-hook 'after-init-hook 'benchmark-init/deactivate))
