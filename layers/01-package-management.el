985459d69c3638bd54cbdf3c4d76ba3e9219e63d
Martin Jungblut Schreiner <martinjungblut@gmail.com>
2025-08-03 18:13:19 -0300

    Move to straight.el.

--- layers/01-package-management.el
+++ layers/01-package-management.el
@@ -1,42 +1,20 @@
-;; declare required packages
-(setq package-list '())
-(add-to-list 'package-list 'use-package)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el"
+                         user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-;; enable package management
-(require 'package)
-(package-initialize)
-
-;; declare package repositories
-(add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
-
-;; update all package repositories unless all required packages are installed and archive contents are available
-(setq refresh-contents-needed nil)
-(unless package-archive-contents
-  (setq refresh-contents-needed t))
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (setq refresh-contents-needed t)))
-(if refresh-contents-needed (package-refresh-contents))
-
-;; install all required packages automatically
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-;; install straight.el
-;; (defvar bootstrap-version)
-;; (let ((bootstrap-file
-;;        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-;;       (bootstrap-version 5))
-;;   (unless (file-exists-p bootstrap-file)
-;;     (with-current-buffer
-;;         (url-retrieve-synchronously
-;;          "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-;;          'silent 'inhibit-cookies)
-;;       (goto-char (point-max))
-;;       (eval-print-last-sexp)))
-;;   (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t) ;; auto use straight.el in every use-package
+(require 'use-package)
 
 (use-package benchmark-init
   :ensure t
